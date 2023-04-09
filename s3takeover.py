7c31b774486191617031ab742099b92a7d6917c0
HwMex0 <102358575+HwMex0@users.noreply.github.com>
2023-04-09 15:20:08 +0300

    Added subdomain enumerating capabilities

--- s3takeover.py
+++ s3takeover.py
@@ -1,5 +1,6 @@
 import requests
 import argparse
+from googlesearch import search
 
 
 def print_banner():
@@ -18,7 +19,17 @@ def read_domains_from_file(filename):
     with open(filename, 'r') as file:
         return [line.strip() for line in file]
 
-def check_domains(domains, timeout):
+def enumerate_subdomains(domain, stop, timeout):
+    print('Enumerating subdomains.. \n')
+    subdomains = []
+    for url in search(f"site:{domain}", stop=stop):
+        subdomain = url.split("//")[-1].split(".")[0]
+        if subdomain not in subdomains:
+            subdomains.append(subdomain)
+
+    return [f"{subdomain}.{domain}" for subdomain in subdomains]
+
+def check_subdomains(domains, timeout):
     matched_domains = []
     non_matched_domains = []
     errored_domains = []
@@ -35,10 +46,13 @@ def check_domains(domains, timeout):
 
     return matched_domains, non_matched_domains, errored_domains
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Check domains for possible S3 takeover (subdomain takeover).")
-    parser.add_argument('-f', '--file', help='Path to the file containing domains, one per line.', required=True)
-    parser.add_argument('-t', '--timeout', help='Timeout value for domain requests in seconds.', type=float, default=2)
+    parser.add_argument('-f', '--file', help='Path to the file containing domains, one per line.')
+    parser.add_argument('-t', '--timeout', help='Timeout value for domain enumeration requests in seconds.', type=float, default=2)
+    parser.add_argument('-d', '--domain', help='Enumerate domain for subdomains (Google Dork)')
+    parser.add_argument('-s', '--stop', type=int, default=20, help='number of search results to be returned')
     return parser.parse_args()
 
 def print_results(matched_domains, non_matched_domains, errored_domains):
@@ -59,6 +73,13 @@ if __name__ == "__main__":
     args = parse_arguments()
     filename = args.file
     timeout = args.timeout
-    domains = read_domains_from_file(filename)
-    matched_domains, non_matched_domains, errored_domains = check_domains(domains, timeout)
-    print_results(matched_domains, non_matched_domains, errored_domains)
+    if args.file:
+        domains = read_domains_from_file(filename)
+        matched_domains, non_matched_domains, errored_domains = check_subdomains(domains, timeout)
+        print_results(matched_domains, non_matched_domains, errored_domains)
+    elif args.domain:
+        domains = enumerate_subdomains(args.domain, args.stop, timeout)
+        matched_domains, non_matched_domains, errored_domains = check_subdomains(domains, timeout)
+        print_results(matched_domains, non_matched_domains, errored_domains)
+    else:
+        print('--domain or --file arguments are required')
